98850b3726ef9e142ef2700f35fffb737df5544d
James Loh <git@jloh.co>
2022-12-10 17:28:44 +1100

    Initial commit of working bot

---
+++ bot.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/mattn/go-mastodon"
+	"github.com/peterhellberg/hn"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func createToot(item *hn.Item, db *sql.DB, tooter *mastodon.Client) (*mastodon.Status, error) {
+	// Create a toot
+	var tootText string
+	if strings.Contains(item.URL, "https://news.ycombinator.com/item?id=") {
+		tootText = fmt.Sprintf("%s\nL: %s", item.Title, item.URL)
+	} else {
+		tootText = fmt.Sprintf("%s\nL: %s\nC: https://news.ycombinator.com/item?id=%v", item.Title, item.URL, item.ID)
+	}
+	toot, err := tooter.PostStatus(context.Background(), &mastodon.Toot{
+		Status: tootText,
+	})
+
+	if err != nil {
+		return toot, err
+	} else {
+		return toot, nil
+	}
+}
+
+func handleItem(pos int, id int, hn *hn.Client, tooter *mastodon.Client, db *sql.DB) {
+	item, err := hn.Item(id)
+	if err != nil {
+		panic(err)
+	}
+	var toot_id int
+	stmt, err := db.Prepare("SELECT toot_id FROM posts where hn_id=?")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	// Check if we have any existing IDs
+	err = stmt.QueryRow(id).Scan(&toot_id)
+	if err != nil {
+		switch {
+		case err == sql.ErrNoRows:
+			log.Printf("Creating toot for: %v\n", item.ID)
+			toot, err := createToot(item, db, tooter)
+			if err != nil {
+				log.Printf("Failed sending toot: %v\n", err)
+			} else {
+				log.Printf("Created toot for item %v (%s/@hackernews/%v)", item.ID, os.Getenv("TOOT_SERVER"), toot.ID)
+				// Save our toot in the DB
+				_, err := db.Exec(`INSERT INTO posts (
+					hn_id, toot_id, posted_at) VALUES(?,?,?)`, item.ID, toot.ID, toot.CreatedAt)
+				if err != nil {
+					log.Printf("Failed saving toot to DB: %v\n", err)
+				}
+			}
+		default:
+			log.Printf("Failed getting rows: %s\n", err)
+		}
+	} else {
+		log.Printf("Already tooted %v at pos %v (%s/@hackernews/%v)", id, pos, os.Getenv("TOOT_SERVER"), toot_id)
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", os.Getenv("DSN"))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("failed to ping: %v", err)
+	}
+
+	log.Println("Successfully connected to DB")
+
+	tooter := mastodon.NewClient(&mastodon.Config{
+		Server:      os.Getenv("TOOT_SERVER"),
+		AccessToken: os.Getenv("TOOT_TOKEN"),
+	})
+
+	hn := hn.DefaultClient
+
+	ids, err := hn.TopStories()
+	if err != nil {
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	for i, id := range ids[:30] {
+		wg.Add(1)
+		go func(id int, pos int) {
+			defer wg.Done()
+			handleItem(pos, id, hn, tooter, db)
+		}(id, i+1)
+	}
+
+	wg.Wait()
+}
---
+++ go.mod
@@ -0,0 +1,14 @@
+module go.jloh.dev/hackernews_bot
+
+go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/mattn/go-mastodon v0.0.6
+	github.com/peterhellberg/hn v0.0.0-20200407070403-5537ecc08ef1
+)
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/tomnomnom/linkheader v0.0.0-20180905144013-02ca5825eb80 // indirect
+)
